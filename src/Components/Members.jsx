573eb3d857f119199769ff7b56f47ec7d1079494
Tanmoy Choudhury <official.tanmoychoudhury@protonmail.com>
2023-07-08 22:41:35 +0530

    changelog: 1. Added new image props in public 2. Finished the Member cards template

--- src/Components/Members.jsx
+++ src/Components/Members.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { BsFillArrowLeftCircleFill } from "react-icons/bs";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+} from "react-icons/fa";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -20,12 +26,11 @@ const Members = () => {
       <Swiper
         effect={"coverflow"}
         slidesPerView={"auto"}
-        loop={true}
         spaceBetween={0}
         navigation={true}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 4000,
           disableOnInteraction: false,
         }}
         coverflowEffect={{
@@ -37,29 +42,66 @@ const Members = () => {
         modules={[Autoplay, EffectCoverflow, Navigation]}
         className="w-full"
       >
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/convenor.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/convenor.jpeg"}
+            name={"Prof. Kuntala Das"}
+            designation={"convener"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/coordinator.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/coordinator.jpeg"}
+            name={"Dr. Madhusudan Debnath"}
+            designation={"coordinator"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/coordinator.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/coordinator.jpeg"}
+            name={"Shreyan Dey"}
+            designation={"General Secretary"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/ags1.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/ags1.jpeg"}
+            name={"Simran Samanta"}
+            designation={"Asst General Secretary"}
+            // git={"sae"}
+            link={"https://www.linkedin.com/in/simran-samanta-563993223"}
+            // tweet={"zxvvsd"}
+            face={"https://www.facebook.com/profile.php?id=100010407793894"}
+            insta={"https://instagram.com/simran.samanta"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/ags2.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/ags2.jpeg"}
+            name={"Shivam Sinha"}
+            designation={"Asst General Secretary"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/cs.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/cs.jpeg"}
+            name={"Pratim Sen Sharma"}
+            designation={"Cultural Secretary"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/acs1.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/acs1.jpeg"}
+            name={"Pratisruti Chatterjee"}
+            designation={"Asst Cultural Secretary"}
+          />
         </SwiperSlide>
-        <SwiperSlide className="bg-red-500 h-96 w-72 pt-4 rounded-2xl">
-          <CardUi pic={"/team/acs2.jpeg"} />
+        <SwiperSlide className="h-fit w-fit rounded-2xl">
+          <CardUi
+            pic={"/team/acs2.jpeg"}
+            name={"Srish Bannerjee"}
+            designation={"Asst Cultural Secretary"}
+          />
         </SwiperSlide>
       </Swiper>
     </div>
@@ -68,12 +110,62 @@ const Members = () => {
 
 const CardUi = (props) => {
   return (
-    <div className="h-full w-full">
-      <img
-        className="rounded-full w-3/4 mx-auto border-8"
-        src={props.pic}
-        alt=""
-      />
+    <div className="bg-violet-300 hover:bg-violet-200 rounded-2xl h-full w-full pt-4 pb-4 transition-all duration-500 group">
+      <div className="rounded-full h-48 w-48 mb-4 ml-10 mr-10 border-8 border-violet-400 overflow-hidden">
+        <img
+          className="rotate-6 group-hover:rotate-0 group-hover:scale-125 transition-all duration-500"
+          src={props.pic}
+          alt=""
+        />
+      </div>
+      <h1 className="text-xs tracking-wide mb-0 uppercase">
+        {props.designation}
+      </h1>
+      {/* <img className="w-60 mt-0 mx-auto" src="divider.png" alt="" /> */}
+      <img className="w-60 mt-0 mx-auto" src="divider.png" alt="" />
+      <h1 className="mb-4 text-lg tracking-wide">{props.name}</h1>
+      <div className="flex justify-center gap-2 mb-4 text-violet-500">
+        <a
+          href={props.git}
+          className={`${
+            props.git ? "felx" : "hidden"
+          } text-4xl hover:transition-all hover:scale-110 hover:-translate-y-1 transition-all duration-500`}
+        >
+          <FaGithub />
+        </a>
+        <a
+          href={props.link}
+          className={`${
+            props.link ? "felx" : "hidden"
+          } text-4xl hover:transition-all hover:scale-110 hover:-translate-y-1 transition-all duration-500`}
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href={props.tweet}
+          className={`${
+            props.tweet ? "felx" : "hidden"
+          } text-4xl hover:transition-all hover:scale-110 hover:-translate-y-1 transition-all duration-500`}
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href={props.face}
+          className={`${
+            props.face ? "felx" : "hidden"
+          } text-4xl hover:transition-all hover:scale-110 hover:-translate-y-1 transition-all duration-500`}
+        >
+          <FaFacebook />
+        </a>
+        <a
+          href={props.insta}
+          className={`${
+            props.insta ? "felx" : "hidden"
+          } text-4xl hover:transition-all hover:scale-110 hover:-translate-y-1 transition-all duration-500`}
+        >
+          <FaInstagram />
+        </a>
+      </div>
     </div>
   );
 };
